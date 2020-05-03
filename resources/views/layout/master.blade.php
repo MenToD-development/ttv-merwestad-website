6dfa1f7cf37d1424e23f340e0404759837ebefcc
Menno Tempelaar <menno@fourdesign.nl>
2020-05-03 18:05:23 +0200

    Zet de standaard html en css op

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  @yield('seo')
+
+  <link href="{{ mix('/css/app.css') }}" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <div id="website" class="bg-white min-w-full min-h-screen">
+    <div class="bg-white w-full h-24 shadow-sm">
+      <div class="container mx-auto flex items-center justify-between h-full">
+        <a class="inline-block" href="{{ route('home') }}">
+          <span class="font-serif font-bold text-xl text-black">t.t.v.</span>
+          <span class="font-serif font-bold text-3xl text-primary-normal">Merwestad</span>
+        </a>
+        <a class="bg-primary-normal hover:bg-primary-hover h-10 inline-flex items-center justify-center px-4 rounded" href="mailto:webmaster@ttvmerwestad.nl">
+          <span class="inline-block font-sans font-bold text-lg text-white">Stuur een e-mail</span>
+        </a>
+      </div>
+    </div>
+      @yield('content')
+  </div>
+  <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,31 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [],
+  purge: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['PT Serif', 'sans-serif'],
+      sans: ['Helvetica', 'sans-serif']
+    },
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.purple,
+      primary: {
+        normal: '#267DF4',
+        hover: '#0B5AC6'
+      },
+
+    },
+    extend: {
+
+    },
   },
   variants: {},
   plugins: [],
