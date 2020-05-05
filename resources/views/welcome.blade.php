a226762fd9210d253963fa4be50af91b22c7ca6b
Menno Tempelaar <menno@fourdesign.nl>
2020-05-05 13:50:43 +0200

    tekst ingevuld voor op de home pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,21 @@
   <div class="bg-white w-full py-16">
     <div class="container mx-auto flex items-stretch">
       <div class="w-2/6 px-5">
-        <h1 class="font-serif text-black">
+        <h1 class="font-serif text-black leading-none mb-6">
           <span class="text-2xl">Tafeltennisvereniging</span>
           <span class="text-5xl text-primary-normal">Merwestad</span>
         </h1>
-        <p class="text-sans text-base text-black">
-          Welkom bij de vereniging
+        <p class="text-sans text-base text-black mb-4">
+          Welkom op de website van tafeltennisvereniging Merwestad. Als vereniging staan wij voor
+          gezelligheid. Onze gymzaal ligt aan de Admiraalsplein te Dordrecht genaamd
+          <a href="https://www.dordtsport.nl/sportlocaties/sport-plus/" target="_blank">Sport+</a>.
+        </p>
+        <p class="text-sans text-base text-black mb-4">
+          Deze website zal informatie verschaffen over <a href="{{ route('news') }}">nieuws</a>,
+          evenementen, competitie wedstrijden & teams, contactgegevens en nog veel meer.
         </p>
       </div>
-      <div class="w-4/6 h-64 px-5">
+      <div class="w-4/6 px-5">
         <div class="w-full h-full overflow-hidden rounded-lg">
           <img class="w-full h-full object-cover" src="/images/groeps-foto.jpg" alt="t.t.v. Merwestad" />
         </div>
