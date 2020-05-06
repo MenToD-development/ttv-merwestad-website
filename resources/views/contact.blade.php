f028df49da17ba8880639ce5fafc89fb73e77822
Menno Tempelaar <menno@fourdesign.nl>
2020-05-06 21:48:57 +0200

    blade file toegevoegd voor de contact pagina

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.master')
+
+@section('seo')
+  <title>Website van t.t.v. Merwestad.</title>
+  <meta name="description" content="t.t.v. Merwestad is een tafeltennisvereniging in Dordrecht.">
+@endsection
+
+@section('content')
+
+@endsection
