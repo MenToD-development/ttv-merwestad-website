aaea3339f34e0bf4c9f9fda3d48a9876d5784ff5
Menno Tempelaar <menno@fourdesign.nl>
2020-05-23 12:11:36 +0200

    created new migration
    Adds the type column to the users table for rights

---
+++ database/migrations/2020_05_23_100910_add_type_column_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->string('type')
+                ->default('web')
+                ->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->removeColumn('type');
+        });
+    }
+}
