9af5725c23658112e38b8ae8449ccfd0e3aa0881
Menno Tempelaar <menno@fourdesign.nl>
2020-05-23 12:16:35 +0200

    added user_types to a new config file

---
+++ config/merwestad.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User types
+    |--------------------------------------------------------------------------
+    |
+    | These values are used for the Select field within the User Resource.
+    | These values determine which rights a user has within the website.
+    */
+    'user_types' => ['web', 'publisher', 'manager', 'admin'],
+
+];
