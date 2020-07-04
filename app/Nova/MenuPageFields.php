46f9cff4c9def4ef60af1b2f5feb840513e02689
Menno Tempelaar <menno@fourdesign.nl>
2020-07-04 15:01:38 +0200

    feat(resource): adds class for the menuPage pivot fields

---
+++ app/Nova/MenuPageFields.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\Number;
+
+class MenuPageFields
+{
+    public function __invoke(): array
+    {
+        return [
+            Number::make('Volgorde', 'order')
+                ->default(function () {
+                    return 1;
+                })
+        ];
+    }
+}
