8e398506bcd3171da9c76e65f224e443b0fdc738
Menno Tempelaar <menno@fourdesign.nl>
2020-07-04 12:08:36 +0200

    adds the menu resource

---
+++ app/Nova/Menu.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+
+class Menu extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Menu::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'name', 'description'
+    ];
+
+    public static $group = 'Website';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Naam', 'name'),
+
+            Textarea::make('Omschrijving', 'description')
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
