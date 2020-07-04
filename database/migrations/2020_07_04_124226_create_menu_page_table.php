18e82672b171e8ce9c31983f42a7699ae10be09a
Menno Tempelaar <menno@fourdesign.nl>
2020-07-04 14:48:43 +0200

    feat(migration): adds menu_page table migration

---
+++ database/migrations/2020_07_04_124226_create_menu_page_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuPageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_page', function (Blueprint $table) {
+            $table->unsignedBigInteger('menu_id');
+            $table->unsignedBigInteger('page_id');
+        });
+
+        Schema::table('menu_page', function (Blueprint $table) {
+           $table->foreign('menu_id')
+            ->on('menus')
+            ->references('id')
+            ->onDelete('cascade');
+
+        $table->foreign('page_id')
+            ->on('pages')
+            ->references('id')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_page');
+    }
+}
