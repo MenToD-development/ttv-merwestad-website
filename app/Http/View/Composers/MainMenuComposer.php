da607c80a2b449d60d8cafcf99b56134290c37f1
Menno Tempelaar <menno@fourdesign.nl>
2020-07-05 11:32:25 +0200

    feat(composer): adds the main-menu composer to the view

--- app/Http/View/Composers/MainMenuComposer.php
+++ app/Http/View/Composers/MainMenuComposer.php
@@ -3,8 +3,28 @@
 
 namespace App\Http\View\Composers;
 
+use Akaunting\Setting\Facade as Setting;
+use App\Menu;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\View;
 
 class MainMenuComposer
 {
+    protected function menu(): Menu
+    {
+        return Menu::where('id', '=', Setting::get('main-menu'))
+            ->get()
+            ->first();
+    }
 
+    protected function pages(): Collection
+    {
+        return $this->menu()->pages
+            ->sortBy('pivot.order');
+    }
+
+    public function compose(View $view): void
+    {
+        $view->with('pages', $this->pages());
+    }
 }
--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\View\Composers\MainMenuComposer;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('layout.partial.navigation', MainMenuComposer::class);
     }
 }
