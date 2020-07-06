79f7138b669cfbdcacd0691b42e1e710bb25d25d
Menno Tempelaar <menno@fourdesign.nl>
2020-07-06 19:42:47 +0200

    feat(view): removed some content for later on from the footer

--- resources/views/layout/partial/footer.blade.php
+++ resources/views/layout/partial/footer.blade.php
@@ -30,67 +30,69 @@
           </a>
         </li>
       </ul>
-      <span class="inline-block text-white text-2xl font-serif mb-4 mt-6">Evenementen</span>
-      <ul>
-        <li class="my-2">
-          <a href="#"
-             class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">
-            Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">
-            Competitie najaar 2020
-          </a>
-        </li>
-      </ul>
+
+{{--      <span class="inline-block text-white text-2xl font-serif mb-4 mt-6">Evenementen</span>--}}
+{{--      <ul>--}}
+{{--        <li class="my-2">--}}
+{{--          <a href="#"--}}
+{{--             class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">--}}
+{{--            Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020--}}
+{{--          </a>--}}
+{{--        </li>--}}
+{{--        <li class="my-2">--}}
+{{--          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">--}}
+{{--            Competitie najaar 2020--}}
+{{--          </a>--}}
+{{--        </li>--}}
+{{--      </ul>--}}
+
     </div>
     <div class="w-full mb-8 md:mb-0 md:w-1/2 lg:w-1/4">
-      <span class="inline-block text-white text-2xl font-serif mb-4">Competitie</span>
-      <ul>
-        <li class="my-2">
-          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">
-            Onderlinge competities
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">
-            Uitslagen
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">
-            Tussenstand
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">
-            Teams
-          </a>
-          <ul>
-            <li class="my-2 ml-5">
-              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">
-                Merwestad 1
-              </a>
-            </li>
-            <li class="my-2 ml-5">
-              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">
-                Merwestad 2
-              </a>
-            </li>
-            <li class="my-2 ml-5">
-              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">
-                Merwestad 3
-              </a>
-            </li>
-            <li class="my-2 ml-5">
-              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">
-                Merwestad 4
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+{{--      <span class="inline-block text-white text-2xl font-serif mb-4">Competitie</span>--}}
+{{--      <ul>--}}
+{{--        <li class="my-2">--}}
+{{--          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">--}}
+{{--            Onderlinge competities--}}
+{{--          </a>--}}
+{{--        </li>--}}
+{{--        <li class="my-2">--}}
+{{--          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">--}}
+{{--            Uitslagen--}}
+{{--          </a>--}}
+{{--        </li>--}}
+{{--        <li class="my-2">--}}
+{{--          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">--}}
+{{--            Tussenstand--}}
+{{--          </a>--}}
+{{--        </li>--}}
+{{--        <li class="my-2">--}}
+{{--          <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans font-bold hover:text-opacity-50">--}}
+{{--            Teams--}}
+{{--          </a>--}}
+{{--          <ul>--}}
+{{--            <li class="my-2 ml-5">--}}
+{{--              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">--}}
+{{--                Merwestad 1--}}
+{{--              </a>--}}
+{{--            </li>--}}
+{{--            <li class="my-2 ml-5">--}}
+{{--              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">--}}
+{{--                Merwestad 2--}}
+{{--              </a>--}}
+{{--            </li>--}}
+{{--            <li class="my-2 ml-5">--}}
+{{--              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">--}}
+{{--                Merwestad 3--}}
+{{--              </a>--}}
+{{--            </li>--}}
+{{--            <li class="my-2 ml-5">--}}
+{{--              <a href="#" class="inline-block w-4/5 truncate text-white text-base font-sans hover:text-opacity-50">--}}
+{{--                Merwestad 4--}}
+{{--              </a>--}}
+{{--            </li>--}}
+{{--          </ul>--}}
+{{--        </li>--}}
+{{--      </ul>--}}
     </div>
     <div class="w-full md:w-1/2 lg:w-1/4">
       <span class="inline-block text-white text-2xl font-serif mb-4">Locatie</span>
