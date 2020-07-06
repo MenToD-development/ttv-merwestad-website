8fe7c72d2edcb5ae3d3c7424c1942ae95e0b8100
Menno Tempelaar <menno@fourdesign.nl>
2020-07-06 19:52:42 +0200

    feat(view): adds text to copyright.blade.php

---
+++ resources/views/layout/partial/copyright.blade.php
@@ -0,0 +1,28 @@
+<div class="w-full bg-white">
+  <div class="container mx-auto px-4 lg:px-8">
+    <div class="flex flex-col md:flex-row">
+      <div class="w-full md:w-1/2 h-16 flex items-center justify-start">
+        <span class="block w-full text-center md:w-auto md:inline-block md:text-left
+        font-sans text-sm
+          text-black">
+          &copy; 2020 T.t.v. Merwestad - Gerealiseerd door
+          <a href="https://github.com/mennotempelaar" title="Github: mennotempelaar (Menno Tempelaar)"
+             target="_blank">Menno Tempelaar</a> en mede mogelijk door
+          <a href="https://nova.laravel.com/" title="Laravel Nova - Beautifully-designed administration panel for Laravel"
+             target="_blank">Laravel Nova
+        </span>
+      </div>
+      <div class="w-full md:w-1/2 h-16 flex items-center justify-end">
+        <a class="inline-block font-sans text-sm text-black px-3" href="#">
+          Disclaimer
+        </a>
+        <a class="inline-block font-sans text-sm text-black px-3" href="#">
+          Privacyverklaring
+        </a>
+        <a class="inline-block font-sans text-sm text-black px-3" href="#">
+          Cookies
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
