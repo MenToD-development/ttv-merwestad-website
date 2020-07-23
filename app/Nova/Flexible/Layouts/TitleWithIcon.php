22dc4111e3e4c640711713b01444bd6e9c70ca02
Menno Tempelaar <menno@fourdesign.nl>
2020-07-23 11:54:13 +0200

    Added Title with Icon

---
+++ app/Nova/Flexible/Layouts/TitleWithIcon.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Nova\Flexible\Layouts;
+
+use Illuminate\Support\Collection;
+use App\Nova\Flexible\Layouts\Icon;
+use Whitecube\NovaFlexibleContent\Layouts\Layout;
+
+class TitleWithIcon extends Layout
+{
+    /**
+     * The layout's unique identifier
+     *
+     * @var string
+     */
+    protected $name = 'title-with-icon';
+
+    /**
+     * The displayed title
+     *
+     * @var string
+     */
+    protected $title = 'Titel met een icoon';
+
+    /**
+     * Get the fields displayed by the layout.
+     *
+     * @return array
+     */
+    public function fields($prefix = '')
+    {
+        $fields = new Collection();
+
+        $fields = $fields->merge(
+            (new Icon())->fields($prefix)
+        );
+
+        $fields->add(
+            \Laravel\Nova\Fields\Text::make('Titel', `{$prefix}title`)
+        );
+
+        return $fields->toArray();
+    }
+
+}
