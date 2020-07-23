807040a5dc553d44fe23b65e764aec1dfa1674cc
Menno Tempelaar <menno@fourdesign.nl>
2020-07-23 11:48:23 +0200

    adds icon layout

---
+++ app/Nova/Flexible/Layouts/Icon.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Nova\Flexible\Layouts;
+
+use Laravel\Nova\Fields\Select;
+use Whitecube\NovaFlexibleContent\Layouts\Layout;
+
+class Icon extends Layout
+{
+    /**
+     * The layout's unique identifier
+     *
+     * @var string
+     */
+    protected $name = 'icon';
+
+    /**
+     * The displayed title
+     *
+     * @var string
+     */
+    protected $title = 'Icoon';
+
+    /**
+     * Get the fields displayed by the layout.
+     *
+     * @return array
+     */
+    public function fields($prefix = '')
+    {
+        return [
+            Select::make('Icoon', `{$prefix}icon`)
+                ->options([
+                    'fa-user-circle' => 'Mijn account icoon',
+                    'fa-paper-plane' => 'E-mail icoon',
+                    'fa-route' => 'Navigeren / route icoon',
+                    'fa-calendar' => 'Agenda icoon',
+                    'fa-table-tennis' => 'Tafeltennis icoon',
+                    'fa-users' => 'Groep mensen icoon',
+                    'fa-arrow-alt-circle-right' => 'Pijl naar rechts',
+                    'fa-arrow-alt-circle-left' => 'Pijl naar links',
+                    'fa-arrow-alt-circle-up' => 'Pijl naar boven',
+                    'fa-arrow-alt-circle-down' => 'Pijl naar beneden',
+                    'fa-smile-beam' => 'Glimlachend gezicht',
+                    'fa-calendar-check' => 'Agenda met een vink',
+                    'fa-fire' => 'Vuur icoon'
+                ])
+        ];
+    }
+
+}
