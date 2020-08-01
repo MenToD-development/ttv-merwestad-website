ff40d8fa3979213a3a552418d8ffb28dd8a79609
Menno Tempelaar <menno@fourdesign.nl>
2020-08-01 10:27:10 +0200

    bugfix

--- resources/views/page/layout/button.blade.php
+++ resources/views/page/layout/button.blade.php
@@ -1,4 +1,3 @@
-@dd($layout)
 
 @if ($layout->page_id)
   <a href="{{ route($layout->page_id) }}"
@@ -10,6 +9,16 @@
       {{ $layout->content }}
     </span>
   </a>
+@elseif ($layout->link_naar_pagina)
+  <a href="{{ route($layout->link_naar_pagina) }}"
+    class="bg-primary-normal hover:bg-primary-hover h-12 inline-flex items-center justify-center px-4 rounded mr-2">
+  @if ($layout->icon)
+    <i class="fas {{ $layout->icon }} text-white text-2xl mr-3"></i>
+  @endif
+  <span class="hidden md:inline-block font-sans font-bold text-lg text-white">
+    {{ $layout->button_tekst }}
+  </span>
+  </a>
 @elseif ($layout->external_link)
   <a href="{{ $layout->external_link }}" target="_blank"
      class="bg-primary-normal hover:bg-primary-hover h-12 inline-flex items-center justify-center px-4 rounded mr-2">
@@ -20,4 +29,14 @@
       {{ $layout->content }}
     </span>
   </a>
+@elseif($layout->externe_link)
+<a href="{{ $layout->external_link }}" target="_blank"
+  class="bg-primary-normal hover:bg-primary-hover h-12 inline-flex items-center justify-center px-4 rounded mr-2">
+ @if ($layout->icon)
+   <i class="fas {{ $layout->icon }} text-white text-2xl mr-3"></i>
+ @endif
+ <span class="hidden md:inline-block font-sans font-bold text-lg text-white">
+   {{ $layout->button_tekst }}
+ </span>
+</a>
 @endif
