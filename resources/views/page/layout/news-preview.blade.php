dc71b74886b170ffbe088491a93a0615c112eec4
Menno Tempelaar <menno@fourdesign.nl>
2020-08-12 19:20:31 +0200

    creates the new news-preview.blade.php

---
+++ resources/views/page/layout/news-preview.blade.php
@@ -0,0 +1,128 @@
+<div class="bg-secondary-normal w-full py-16 md:py-24 xl:py-32">
+  <div class="container mx-auto px-4 xl:px-8 flex items-stretch">
+    <div class="w-full">
+      <div class="text-center mb-2">
+        <span class="rounded py-1 px-2 md:py-2 md:px-4 bg-white">
+          <span class="text-xs md:text-sm font-bold font-sans text-primary-normal">
+            Wat gebeurt er bij Merwestad
+          </span>
+        </span>
+      </div>
+      <h2 class="block text-center font-serif text-3xl md:text-4xl text-black mb-10 xl:mb-16">
+        Laatste nieuws
+      </h2>
+      <div class="flex flex-col lg:flex-row lg:items-stretch">
+        <div class="w-full lg:w-2/4 xl:w-1/3 p-3">
+          <div class="bg-white w-auto h-full overflow-y-hidden p-6 relative shadow">
+            <span class="rounded py-1 px-2 md:py-2 md:px-4 bg-secondary-normal">
+              <span class="text-xs md:text-sm font-bold font-sans text-primary-normal">
+                Uitgelicht nieuwsbericht
+              </span>
+            </span>
+            <span class="text-black text-lg md:text-xl font-serif block mt-3">
+              Competitie najaar 2020
+            </span>
+            <span class="block text-primary-normal text-opacity-75 text-xs md:text-sm font-sans mb-6">
+              Stefan de Jong - 2 maanden geleden
+            </span>
+            <div class="h-64 min-h-full mb-6 prose">
+              <p>
+                Ondanks dat het er voorlopig naar uit ziet dat we pas op 1 september
+                weer mogen beginnen, heb ik toch het verzoek gehad van de bond om de
+                teams door te geven voor de najaarscompetitie. Het inschrijven kan tot
+                15 juni 2020.
+              </p>
+              <p>
+                Waarschijnlijk zal zoals het er nu naar uitziet, zullen er geen dubbels
+                worden gespeeld. Voor reguliere competitie betekent het dat er dan 9
+                wedstrijden gespeeld worden. Bij de Duo is momenteel de intentie om een
+                extra enkel te spelen.
+              </p>
+              <p>
+                <strong>
+                  Graag hoor ik wie of welke teams competitie willen spelen.
+                </strong></p>
+              <p>
+                De competitie kan regulier zijn, dit betekent een team van 3 spelers die
+                elk 3 wedstrijden spelen en (een dubbel) , dit zijn 10 wedstrijden per
+                avond. (nu waarschijnlijk 9 stuks)
+              </p>
+              <p>
+                Ook kan dit duo zijn, dit is een team van 2 spelers, die elk 2
+                wedstrijden spelen en (een dubbel), dit zijn 5 wedstrijden, voordeel is
+                dat het niet zo laat wordt.
+              </p>
+              <p>
+                Mocht de 1,5 meter nog steeds van toepassing zijn, dan dien je er ook
+                rekening mee te houden dat vervoer van een team in één auto vermoedelijk
+                moeilijk of niet mogelijk is.
+              </p>
+            </div>
+            <div class="absolute bottom-0 left-0 w-full bg-white p-6">
+              <a href="#" title="Nieuwsbericht: Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020" class="bg-primary-normal hover:bg-primary-hover h-10 inline-flex items-center justify-center px-4 rounded">
+                <i class="fas fa-arrow-alt-circle-right text-white text-md md:text-lg mr-3"></i>
+                <span class="inline-block font-sans font-bold text-base text-white">
+                  Lees verder
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="w-full lg:w-2/4 xl:w-2/3 p-3">
+          <div class="bg-white p-6 w-full mb-6 shadow">
+            <span class="text-black text-lg md:text-xl font-serif block">
+              Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020
+            </span>
+            <span class="block text-primary-normal text-opacity-75 text-xs md:text-sm font-sans mb-6">
+              Kirsten Bubbert - 2 maanden geleden
+            </span>
+            <p class="inline-block font-sans text-base text-black mb-4">
+              Naar aanleiding van de laatste ontwikkelingen en de aanwijzingen vanuit de RIVM en
+              de overheid dienen de gemeentelijke sportaccommodaties gesloten te blijven tot en
+              met maandag 31 augustus 2020.
+            </p>
+            <a href="#"
+              title="Nieuwsbericht: Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020"
+              class="bg-primary-normal hover:bg-primary-hover h-10 inline-flex items-center justify-center px-4 rounded">
+              <i class="fas fa-arrow-alt-circle-right text-white text-md md:text-lg mr-3"></i>
+              <span class="inline-block font-sans font-bold text-base text-white">
+                Lees verder
+              </span>
+            </a>
+          </div>
+          <div class="bg-white p-6 w-full shadow">
+            <span class="text-black text-lg md:text-xl font-serif block">
+              Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020
+            </span>
+            <span class="block text-primary-normal text-opacity-75 text-xs md:text-sm font-sans mb-6">
+              Kirsten Bubbert - 2 maanden geleden
+            </span>
+            <p class="inline-block font-sans text-base text-black mb-4">
+              Naar aanleiding van de laatste ontwikkelingen en de aanwijzingen vanuit de RIVM en
+              de overheid dienen de gemeentelijke sportaccommodaties gesloten te blijven tot en
+              met maandag 31 augustus 2020.
+            </p>
+            <a href="#"
+              title="Nieuwsbericht: Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020"
+              class="bg-primary-normal hover:bg-primary-hover h-10 inline-flex items-center justify-center px-4 rounded">
+              <i class="fas fa-arrow-alt-circle-right text-white text-md md:text-lg mr-3"></i>
+              <span class="inline-block font-sans font-bold text-base text-white">
+                Lees verder
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center mt-10">
+        <a href="#"
+          title="Nieuwsbericht: Gemeentelijke binnensportaccomodaties gesloten t/m 31-08-2020"
+          class="bg-primary-normal hover:bg-primary-hover h-10 md:h-12 inline-flex items-center justify-center px-4 rounded">
+          <i class="fas fa-newspaper text-white mr-3"></i>
+          <span class="inline-block font-sans font-bold text-md md:text-lg text-white">
+            Bekijk meer nieuws
+          </span>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
