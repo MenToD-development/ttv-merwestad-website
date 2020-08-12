9c82edb0511488c5d18292f065db064f2ab99282
Menno Tempelaar <menno@fourdesign.nl>
2020-08-12 20:45:29 +0200

    adds a function for collecting the rest of the posts

--- app/Nova/Flexible/Layouts/NewsPreview.php
+++ app/Nova/Flexible/Layouts/NewsPreview.php
@@ -3,6 +3,8 @@
 namespace App\Nova\Flexible\Layouts;
 
 use App\Post;
+use Illuminate\Database\Eloquent\Collection;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Whitecube\NovaFlexibleContent\Layouts\Layout;
 
@@ -37,17 +39,44 @@ class NewsPreview extends Layout
         return [
             Text::make('Label'),
 
-            Text::make('Titel', 'title')
+            Text::make('Titel', 'title'),
+
+            Select::make('Uitgelicht nieuwsbericht', 'featured')
+                ->options($this->postsAsOptions())
         ];
     }
 
+    /**
+     * @return mixed
+     */
     protected function postsAsOptions()
     {
         return Post::published()
-            ->all()
+            ->get()
             ->mapWithKeys(function (Post $post) {
                 return [$post->id => $post->title];
-            });
+            })
+            ->toArray();
     }
 
+    /**
+     * @param int $id
+     * @return Post
+     */
+    public function post(int $id): Post
+    {
+        return Post::with(['author'])->find($id);
+    }
+
+    /**
+     * @param int $id
+     * @param int $max
+     * @return Collection
+     */
+    public function postsWithout(int $id, int $max = 3): Collection
+    {
+        return Post::published()
+            ->where('id', '!=', $id)
+            ->get();
+    }
 }
