d60c6bcfbeb8edaff3e8839f0e1fb88f500dd1ef
Menno Tempelaar <menno@fourdesign.nl>
2020-10-20 14:07:45 +0200

    Openingstijden aangepast

--- resources/views/page/layout/contact-information.blade.php
+++ resources/views/page/layout/contact-information.blade.php
@@ -103,7 +103,7 @@
           Dinsdag
         </span>
         <span class="inline-block font-sans text-black w-2/3 text-base xl:text-lg">
-          19:30 - 23:00
+          19:30 - 22:00
         </span>
       </div> 
       <div class="flex">
